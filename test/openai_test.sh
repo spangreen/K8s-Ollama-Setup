7dafa5ef8eb848a9b56b1a6d745bd0b61b355818
Alan Greenspan <spangreen@gmail.com>
2025-05-27 12:39:51 -0400

    switch to using Ingress

--- test/openai_test.sh
+++ test/openai_test.sh
@@ -1,3 +1,5 @@
 #/bin/sh
 
-curl http://llm-host:31134/api/chat -d '{ "model": "tinyllama", "stream": false, "messages": [ { "role": "user", "content": "Write a Python function that reverses a string." } ] }' | jq -r '.message.content'
+#curl http://llm-host:80/api/chat -d '{ "model": "tinyllama", "stream": false, "messages": [ { "role": "user", "content": "Write a Python function that reverses a string." } ] }' | jq -r '.message.content'
+
+curl http:/llm-host/v1/models | jq '.data[].id'
