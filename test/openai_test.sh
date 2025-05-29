e26692fd1870e2136dacf46553cf731717b2900a
Alan Greenspan <spangreen@gmail.com>
2025-05-29 15:43:11 -0400

    switch to qwen model

--- test/openai_test.sh
+++ test/openai_test.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-#curl -s http://ollama.llm-host/v1/chat/completions -d '{ "model": "tinyllama", "stream": false, "messages": [ { "role": "user", "content": "Write a Python function that reverses a string." } ] }' | jq -r '.message.content'
+#curl -s http://ollama.llm-host/v1/chat/completions -d '{ "model": "tinyllama", "stream": false, "messages": [ { "role": "user", "content": "Write a small Python program that reverses a string."} ] }' | jq '.choices[0].message.content'
 
 curl -s http://ollama.llm-host/v1/models | jq '.data[].id'
